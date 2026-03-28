a742118fe61251146e43a5e2164a35edf705b265
Eitan Ostashinski <eitanoid.os@gmail.com>
2026-03-28 12:08:59 +0000

    abstracted input and added input from pipes and redirects

--- cmd/root.go
+++ cmd/root.go
@@ -2,10 +2,12 @@ package cmd
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"strings"
 
+	"github.com/eitanoid/toml-resume/internal/loader"
 	"github.com/eitanoid/toml-resume/internal/resume"
 	"github.com/spf13/cobra"
 )
@@ -27,24 +29,46 @@ func Execute() {
 var rootCmd = &cobra.Command{
 	Use:   "resume-gen [input file]",
 	Short: "Generate a LaTeX resume from TOML/YAML",
-	Args:  cobra.ExactArgs(1), // ensures exactly one input file
+	Args:  cobra.MaximumNArgs(1), // ensures exactly one input file
 	RunE: func(cmd *cobra.Command, args []string) error {
-		inputPath := args[0]
-
-		// resolve output file, infer input.tex if not specified
 		finalOutput := outputFile
-		if finalOutput == "" {
-			ext := filepath.Ext(inputPath)
-			finalOutput = strings.TrimSuffix(inputPath, ext) + ".tex"
+		var r io.Reader
+
+		// read from file if input was provided
+		if len(args) > 0 && args[0] != "-" {
+			inputPath := args[0]
+
+			// resolve output file, infer input.tex if not specified
+			if finalOutput == "" {
+				ext := filepath.Ext(inputPath)
+				finalOutput = strings.TrimSuffix(inputPath, ext) + ".tex"
+			}
+
+			// open file as a reader
+			var err error
+			if r, err = os.Open(inputPath); err != nil {
+				return fmt.Errorf("failed to open file: %w", err)
+			}
+
+		} else {
+			// determine if input is provided from pipe or redirection
+			stat, _ := os.Stdin.Stat()
+			if (stat.Mode() & os.ModeCharDevice) == 0 {
+				r = os.Stdin
+			} else {
+				// no pipe and no file argument
+				return fmt.Errorf("no input provided via pipe or file argument")
+			}
 		}
 
 		resume := resume.NewDefaultResume()
-		if err := resume.LoadDataFromFile(inputPath); err != nil {
-			return err
+
+		if err := loader.LoadFromReader(r, resume.Data); err != nil {
+			return fmt.Errorf("failed to load config data: %w", err)
 		}
 		resume.CreateLatexDoc()
 
-		if finalOutput == "-" {
+		if finalOutput == "-" || finalOutput == "" {
 			// write to stdout
 			fmt.Print(resume.String())
 		} else {
@@ -64,45 +88,3 @@ func init() {
 	// Define the output flag. Default is empty string to trigger inference logic.
 	rootCmd.Flags().StringVarP(&outputFile, "output", "o", "", "output path (use '-' for stdout)")
 }
-
-// func main() {
-// 	var inputPath string
-// 	var outputPath string
-//
-// 	if len(os.Args) > 1 {
-// 		inputPath = os.Args[1]
-// 	}
-//
-// 	if inputPath == "" {
-// 		fmt.Fprint(os.Stderr, ErrNoInput)
-// 		os.Exit(1)
-// 	}
-//
-// 	outputPath = strings.TrimSuffix(inputPath, filepath.Ext(inputPath)) + ".tex"
-//
-// 	resume := NewDefaultResume()
-// 	if err := resume.loadDataFromFile(inputPath); err != nil {
-// 		fmt.Fprintf(os.Stderr, "failed to load data from specified file: %v", err)
-// 	}
-// 	if err := resume.ValidateConfig(); err != nil {
-// 		fmt.Fprintf(os.Stderr, "invalid configuration: %v", err)
-// 	}
-//
-// 	resume.CreateLatexDoc()
-//
-// 	if outputPath != "" {
-// 		f, err := os.Create(outputPath)
-// 		if err != nil {
-// 			fmt.Fprintf(os.Stderr, "failed to create output file \"%s\": %v", outputPath, err)
-// 			os.Exit(1)
-// 		}
-// 		defer f.Close()
-// 		if _, err := f.WriteString(resume.String()); err != nil {
-// 			fmt.Fprintf(os.Stderr, "failed to write to output file \"%s\": %v", outputPath, err)
-// 			os.Exit(1)
-// 		}
-// 	} else {
-// 		fmt.Println(resume.String())
-// 	}
-//
-// }
