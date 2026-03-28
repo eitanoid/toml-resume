1727f94911df54e406359b4fcad83f47970aff0c
Eitan Ostashinski <eitanoid.os@gmail.com>
2026-03-28 19:24:07 +0000

    delete old run file

--- makecv.sh
+++
@@ -1,5 +0,0 @@
-docker run \
-    -v $(pwd):/data:z \
-    -v /usr/share/fonts:/usr/share/fonts/sysfonts:ro,z \
-    -v $HOME/.local/share/fonts:/usr/share/fonts/userfonts:ro,z \
-    toml-resume "$@"
